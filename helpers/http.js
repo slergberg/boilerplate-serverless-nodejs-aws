48eaff3becf444d1037642b08b525746aea873b1
Rodrigo Scomação do Nascimento <rodrigon@rpc.com.br>
2018-08-09 16:43:08 -0300

    Adicionado suporte a CORS

--- helpers/http.js
+++ helpers/http.js
@@ -6,18 +6,30 @@ const formatErrorResponse = (error, code = 400) => {
   if (error instanceof ApiError) {
     return {
       statusCode: error.statusCode,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
       body: JSON.stringify(error.details),
     }
   }
 
   return {
     statusCode: code,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
     body: error.message,
   }
 }
 
 const formatResponse = (data, code = 200) => ({
   statusCode: code,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+  },
   body: JSON.stringify(data),
 })
 
